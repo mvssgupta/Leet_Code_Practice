896456cea619cb7e9fb47f64d3499ce5daeb8027
mvssgupta <mvssguptajagadesh@gmail.com>
2022-09-02 18:54:31 +0530

    Time: 32 ms (25.15%), Space: 22.4 MB (72.83%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -11,34 +11,35 @@
  */
 class Solution {
 public:
-    //the logic is , just traverse the binary tree in level wise and
-    //store the items of a level in a vector and create another vector called res vector
-    //which is used to store the average values.
-    //after traversing a level , calculate the average of that level (which is done at line number 39)
-    //and store it in res vector and finally return the res vector as answer.
-    vector<double> averageOfLevels(TreeNode* root) {
-        vector<double> res;
-        if (root == NULL)
-            return res;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty())
-        {
-            int len = q.size();
-            vector<int> levelTraversal;
-            for (int i = 0; i < len; i++)
-            {
-                TreeNode *node = q.front();
-                q.pop();
-                if (node->left!=NULL)
-                    q.push(node->left);
-                if (node->right!=NULL)
-                    q.push(node->right);
-                levelTraversal.push_back(node->val);
-            }
-            double avg = (accumulate(levelTraversal.begin(),levelTraversal.end(),0.0))/levelTraversal.size();
-            res.push_back(avg);
-        }
-        return res;
-    }
+   //the logic is , just traverse the binary tree in level wise and
+   //while traversing calculate the sum using a variable and create a vector called res vector
+   //which is used to store the average values.
+   //after traversing a level , calculate the average of that level (which is sum/count... done at line 41)
+   //and store it in res vector and finally return the res vector as answer.
+   vector<double> averageOfLevels(TreeNode* root) {
+       vector<double> res;
+       if (root == NULL)
+           return res;
+       queue<TreeNode*> q;
+       q.push(root);
+       while (!q.empty())
+       {
+           int len = q.size();
+           double sum=0;
+   		   int count=0;
+           for (int i = 0; i < len; i++)
+           {
+               TreeNode *node = q.front();
+               q.pop();
+               if (node->left!=NULL)
+                   q.push(node->left);
+               if (node->right!=NULL)
+                   q.push(node->right);
+               sum+=node->val;
+   			   count++;
+           }
+           res.push_back(sum/count);
+       }
+       return res;
+   }
 };
