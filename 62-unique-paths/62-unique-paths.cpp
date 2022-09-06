2e4da7a2c3bc3a67b8b99c9635b3faf1ad424313
mvssgupta <mvssguptajagadesh@gmail.com>
2022-09-06 18:46:39 +0530

    Time: 7 ms (6.70%), Space: 10.3 MB (27.04%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int rec(int i, int j , int m , int n)
+    {
+        if (i > m || j > n)
+            return 0;
+        if ((i == m - 1) && (j == n - 1))
+            return 1;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        dp[i][j] = 0;
+        dp[i][j] += rec(i + 1, j,m,n) + rec(i, j + 1,m,n);
+        return dp[i][j];
+    }
+    int uniquePaths(int m, int n) {
+        memset(dp,-1,sizeof(dp));
+        return rec(0,0,m,n);
+    }
+};
