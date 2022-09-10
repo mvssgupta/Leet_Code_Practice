2c9817a6df17329f3099293419827cee46ac7985
mvssgupta <mvssguptajagadesh@gmail.com>
2022-09-10 14:37:25 +0530

    Time: 7 ms (17.11%), Space: 6.4 MB (68.61%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        //by using two pointers we can solve the below problem as follows.
+        //here sm and lr are the pointers according to the constraints s is smaller than t,
+        //so I take sm(small) to traverse string s and lr(large) to traverse string t
+        
+        //Intially both the pointers are at the 0th index , if we found a match we increment
+        //both of them else we increment only the lr pointer.
         int sm=0,lr=0;
         while(sm<s.size() && lr<t.size())
         {
