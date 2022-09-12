38126d523f852f8458f9eea18f09c3cc7a6c61ad
mvssgupta <mvssguptajagadesh@gmail.com>
2022-09-12 23:52:12 +0530

    Time: 3 ms (38.96%), Space: 5.8 MB (77.70%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
-    char reVal(int num)
-    {
-        if (num >= 0 && num <= 9)
-            return (char)(num + '0');
-        else
-            return (char)(num - 10 + 'A');
-    }
-    bool isPalindrome(string s)
-    {
-        for(int i=0;i<(s.size()/2);i++)
-        {
-            if(s[i]!=s[s.size()-i-1]) return false;
-        }
-        return true;
-    }
-    string baseX(int inputNum, int base)
-    {
-        string res = "";
-        while (inputNum > 0) {
-            res += reVal(inputNum % base);
-            inputNum /= base;
-        }
-        reverse(res.begin(), res.end());
-        return res;
-    }
-    bool isStrictlyPalindromic(int n) {
-        bool ans=true;
-        for(int i=2;i<=n-2;i++)
-        {
-            ans = ans && isPalindrome(baseX(n,i));
-        }
-        return ans;
+    //brute force approch
+    
+    // char reVal(int num)
+    // {
+    //     if (num >= 0 && num <= 9)
+    //         return (char)(num + '0');
+    //     else
+    //         return (char)(num - 10 + 'A');
+    // }
+    // bool isPalindrome(string s)
+    // {
+    //     for(int i=0;i<(s.size()/2);i++)
+    //     {
+    //         if(s[i]!=s[s.size()-i-1]) return false;
+    //     }
+    //     return true;
+    // }
+    // string baseX(int inputNum, int base)
+    // {
+    //     string res = "";
+    //     while (inputNum > 0) {
+    //         res += reVal(inputNum % base);
+    //         inputNum /= base;
+    //     }
+    //     reverse(res.begin(), res.end());
+    //     return res;
+    // }
+    // bool isStrictlyPalindromic(int n) {
+    //     bool ans=true;
+    //     for(int i=2;i<=n-2;i++)
+    //     {
+    //         ans = ans && isPalindrome(baseX(n,i));
+    //     }
+    //     return ans;
+    // }
+    
+    //the simple solution would be returning false.
+      bool isStrictlyPalindromic(int n) {
+        return false;
     }
 };
