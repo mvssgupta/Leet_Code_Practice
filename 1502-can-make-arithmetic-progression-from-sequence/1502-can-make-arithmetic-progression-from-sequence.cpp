d4798535b7479a6d50caef46d4dfaf4e80538434
mvssgupta <mvssguptajagadesh@gmail.com>
2022-09-14 22:07:25 +0530

    Time: 4 ms (75.49%), Space: 9.1 MB (35.00%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        
+        //brute force approach -> O(NlogN)time complexity
+        // sort(arr.begin(),arr.end());
+        // int diff = arr[1]-arr[0];
+        // for(int i=0;i<arr.size()-1;i++){
+        //     if(arr[i+1]-arr[i] != diff) return false;
+        // }
+        // return true;
+        
+        //optimized solution->  O(N) time complexity
+        int mn = *min_element(arr.begin(), arr.end()), mx = *max_element(arr.begin(), arr.end()), N = arr.size();
+        if ((mx - mn) % (N - 1)!=0) return false;
+        int diff = (mx - mn) / (N - 1);
+        for (int i = 0; i < N;) {
+            if (arr[i] == mn + i * diff) ++i;
+            else if (((arr[i] - mn) % diff)!=0) return false;
+            else {
+                int j = (arr[i] - mn) / diff;
+                if (j < i || arr[i] == arr[j]) return false;
+                swap(arr[i], arr[j]);
+            }
+        }
+        return true;
+    }
+};
