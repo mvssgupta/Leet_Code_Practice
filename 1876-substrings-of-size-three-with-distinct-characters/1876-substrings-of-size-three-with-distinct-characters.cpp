6c450de35f57e90480a56199f429652602010747
mvssgupta <mvssguptajagadesh@gmail.com>
2022-09-20 23:11:21 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (24.47%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -6,9 +6,9 @@ public:
         int ans=0;
         while(j<s.size()){
             ump[s[j]]++;
-            if(j-i+1==3)
+            if(j-i+1==3)//If we get a window of size 3 then we can check whether it has three distinct characters or not
             {
-                if(ump.size()==3) ans++;
+                if(ump.size()==3) ans++; //If it has three distinct characters , we can increment our ans
                 if(ump[s[i]]==1) ump.erase(s[i]);
                 else ump[s[i]]--;
                 i++;
