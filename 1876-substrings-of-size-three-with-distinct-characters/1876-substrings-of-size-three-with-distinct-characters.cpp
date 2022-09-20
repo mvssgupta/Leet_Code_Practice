43d3c274668c1c71a731198bf1dc773ff3f912f5
mvssgupta <mvssguptajagadesh@gmail.com>
2022-09-20 23:09:20 +0530

    Time: 12 ms (13.64%), Space: 6.5 MB (34.10%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -6,17 +6,14 @@ public:
         int ans=0;
         while(j<s.size()){
             ump[s[j]]++;
-            if(j-i+1<3)
-            {
-                j++;
-            }
-            else if(j-i+1==3)
+            if(j-i+1==3)
             {
                 if(ump.size()==3) ans++;
                 if(ump[s[i]]==1) ump.erase(s[i]);
                 else ump[s[i]]--;
-                i++;j++;
+                i++;
             }
+            j++;
         }
         return ans;
     }
