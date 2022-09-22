2c3c6fa1091d38c85f19244c1840312aa03d2804
mvssgupta <mvssguptajagadesh@gmail.com>
2022-09-22 22:52:26 +0530

    Time: 16 ms (81.08%), Space: 9.7 MB (75.20%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-      int i=0;
-      string ans="";
-      string subs = "";
-        cout<<s.size();
-      while(i<s.size())
-      {
-          if(!isspace(s[i]))
-          {
-              subs+=s[i];
-          }
-          else{
-              reverse(subs.begin(),subs.end());
-              ans+=subs+" ";
-              subs = "";
-          }
-          i++;
-      }
-     reverse(subs.begin(),subs.end());
-    ans+=subs;
-    return ans;
+    //this logic is also same as two pointers but I have used extra strings(ie. ans,subs) to do the problem
+    // string reverseWords(string s) {
+    //   int i=0;
+    //   string ans="";
+    //   string subs = "";
+    //   while(i<s.size())
+    //   {
+    //       if(!isspace(s[i]))
+    //       {
+    //           subs+=s[i];
+    //       }
+    //       else{
+    //           reverse(subs.begin(),subs.end());
+    //           ans+=subs+" ";
+    //           subs = "";
+    //       }
+    //       i++;
+    //   }
+    //  reverse(subs.begin(),subs.end());
+    // ans+=subs;
+    // return ans;
+    // }
+    
+    //using two pointers
+        string reverseWords(string s) {
+        int start=0;
+        for(int end=0;end<s.size();end++){
+            if(s[end]==' ') //increment end pointer until u find a space , if u get a space then reverse the string upto that place.
+            { 
+                reverse(s.begin()+start, s.begin()+end);     //reversing the string upto that place.
+                start=end+1;                                 //updating the start pointer for next word reversal
+            }
+        }
+        reverse(s.begin()+start,s.end());//we have to explicitly reverse the last word,as the last word don't has ' '(space) at the end;
+        return s;
     }
 };
