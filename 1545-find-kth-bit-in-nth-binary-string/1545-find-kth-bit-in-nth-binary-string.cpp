8e548514708cf2fb9f98583e11bd7fb0f3e4747f
mvssgupta <mvssguptajagadesh@gmail.com>
2022-09-26 01:20:21 +0530

    Time: 0 ms (100.00%), Space: 6 MB (71.92%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-    string invert(string s)
-    {
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='0') s[i]='1';
-            else s[i]='0';
-        }
-        return s;
-    }
-    string rec(int n)
-    {
-        if(n==1) return "0";
-        string prev = rec(n-1);
-        string inv = invert(prev);
-        reverse(inv.begin(),inv.end()); 
-        string ans =  prev+"1"+inv;
-        return ans;
-    }
+    /*
+    observe the pattern:-
+    s1 = "0"
+    s2 = "011"
+    s3 = "0111001"
+    
+    we can observe that len(s2) = (2^2) -1 , so for n the length is (2^n)-1
+    and the middle bit is always 1 , and the bits after the mid is exactly inverse,
+    so if k is lesser than mid then we can return the bit at s(n-1)
+    else we have to return inverse of s(n-1)'s (n-1-k)th bit ,  why because look at the following -
+    
+    s3 = 0111001 => s2+1+reverse(s2), here s2 = 011 , so s3 = 011 + 1 + 110 , so if k is greater than mid , 
+    we have to return inverse from begining.
+    */
+    
+    //brute force approach
+    // string invert(string s)
+    // {
+    //     for(int i=0;i<s.size();i++)
+    //     {
+    //         if(s[i]=='0') s[i]='1';
+    //         else s[i]='0';
+    //     }
+    //     return s;
+    // }
+    // string rec(int n)
+    // {
+    //     if(n==1) return "0";
+    //     string prev = rec(n-1);
+    //     string inv = invert(prev);
+    //     reverse(inv.begin(),inv.end()); 
+    //     string ans =  prev+"1"+inv;
+    //     return ans;
+    // }
+    
+
     char findKthBit(int n, int k) {
-        string ans = rec(n);
-        return ans[k-1];
+        // string ans = rec(n);
+        // return ans[k-1];
+        if(n==1) return '0';
+        int ans = (pow(2,n)-1)/2;         //calculating the mid value
+        if(k==ans+1) return '1';     //if k is exactly pointing to the centre bit , we can return 1 , since center bit is always one.
+        if(k<ans+1) return findKthBit(n-1,k);   //if k is less than half size of the string then we can say it value is same as string(n-1).
+        int val = findKthBit(n-1,pow(2,n)-k);
+        return (val=='0')?'1':'0';
     }
 };
