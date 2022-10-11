df1bc22ddf9226560f367b1cbd80aa15b6118737
mvssgupta <mvssguptajagadesh@gmail.com>
2022-10-11 19:24:26 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.65%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int N = palindrome.size();
+        if(N==1) return "";
+        bool flag=false;
+        for(int i=0;i<N/2;i++)
+        {
+            if(palindrome[i]!='a')
+            {
+                palindrome[i]='a';
+                flag=true;
+                break;
+            }
+        }
+        if(!flag) palindrome[N-1]='b';
+        return palindrome;
+    }
+};
