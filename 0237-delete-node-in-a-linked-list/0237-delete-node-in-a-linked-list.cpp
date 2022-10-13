4b84d13715a7144342f2bcee93dc340381653fbc
mvssgupta <mvssguptajagadesh@gmail.com>
2022-10-13 09:28:07 +0530

    Time: 32 ms (28.46%), Space: 7.6 MB (37.95%) - LeetHub

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// we should use our master brain to solve this question\U0001f602.
+//*here we have access to only to the right side of the reference
+//*  1->2->6->8->9
+//*for suppose we are asked to delete 6 then we should break the link
+//*between 2 and 6 and we should make a link to 8 from 2 and then
+//*using delete() operation delete 6
+
+
+//*But it can done by simply coping the value of 8 to 6 (it will become like : 1->2->8->6->9)
+//*and then simply break the link between 6 and 8 and join 8 and 9.
+//*then it looks like 1->2->8->9
+
+class Solution {
+public:
+    void deleteNode(ListNode* refToNode) {
+        ListNode* nextNode = refToNode->next;
+        refToNode->val = nextNode->val;
+        refToNode->next = refToNode->next->next;
+        delete(nextNode);
+    }
+};
